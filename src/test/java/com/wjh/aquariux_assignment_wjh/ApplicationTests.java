e91f127e148f8fd571611ceb1e4f5b32c4b24467
jingheng.wu <wujingheng.j@gmail.com>
2025-01-12 17:01:29 +0800

    initial commit

---
+++ src/test/java/com/wjh/aquariux_assignment_wjh/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjh.aquariux_assignment_wjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
